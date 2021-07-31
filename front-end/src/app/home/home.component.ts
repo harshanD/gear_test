93750b546902273bb13f265d6e9804fd829ef32c
harshan dilantha <harshan.dilantha@gmail.com>
2021-07-31 10:59:44 +0530

    initial stage

---
+++ front-end/src/app/home/home.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthService} from "../services/auth.service";
+import {JwtHelperService} from "../services/jwt-helper.service";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  // Variables
+  accessToken: any;
+  accessTokenDetails: any;
+  loading: boolean;
+  constructor(
+    jwtHelper: JwtHelperService,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.accessToken = localStorage.getItem('access_token');
+    this.accessTokenDetails = {
+      id: jwtHelper.id(),
+      name: jwtHelper.name(),
+      email: jwtHelper.email()
+    };
+  }
+
+  ngOnInit(): void {
+  }
+
+  /**
+   * Logout the user and revoke his token
+   */
+  logout(): void {
+    this.loading = true;
+    this.authService.logout()
+      .subscribe(() => {
+        this.loading = false;
+        localStorage.removeItem('access_token');
+        this.router.navigate(['/login']);
+      });
+  }
+}
